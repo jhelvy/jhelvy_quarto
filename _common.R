dd51dbe232066ad0d16722448133dee497aca24b
John Helveston <john.helveston@gmail.com>
2024-01-29 06:51:39 -0500

    starting to work on moving over posts

--- _common.R
+++ _common.R
@@ -93,10 +93,15 @@ make_media_list <- function() {
     return(paste(temp$post, collapse = "\n"))
 }
 
-make_pub_list <- function(pubs, category) {
-  x <- pubs[which(pubs$category == category),]
-  pub_list <- lapply(split(x, 1:nrow(x)), make_pub)
-  return(paste(unlist(pub_list), collapse = ""))
+make_pub_list <- function(pubs, category, featured = FALSE) {
+    x <- pubs[which(pubs$category == category),]
+    if (featured) {
+        x <- x[which(x$featured),]
+    } else {
+        x <- x[which(!x$featured),]
+    }
+    pub_list <- lapply(split(x, 1:nrow(x)), make_pub)
+    return(paste(unlist(pub_list), collapse = ""))
 }
 
 make_pub <- function(pub) {
